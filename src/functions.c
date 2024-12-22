3d42c9a60da2216f91ffde853d65d0dafd1afb00
Barış Getiren <347hgb@gmail.com>
2024-12-22 22:25:33 +0300

    Update functions.c

--- src/functions.c
+++ src/functions.c
@@ -29,23 +29,73 @@
 
 refera_symbol_t refera_sum(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+    retval.dim = 0; 
+    retval.base = malloc(sizeof(int));
+
+    int sum = 0;
+    if (symbol.dim == 1) {
+        for (int i = 0; i < symbol.size1; i++) {
+            sum += symbol.base[i];
+        }
+    } else if (symbol.dim == 2) {
+        for (int i = 0; i < symbol.size1; i++) {
+            for (int j = 0; j < symbol.size2; j++) {
+                sum += symbol.base[i * symbol.size2 + j];
+            }
+        }
+    }
+
+    *retval.base = sum;
+    return retval;
 }
 
 refera_symbol_t refera_aver(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+    retval.dim = 0; 
+    retval.base = malloc(sizeof(int));
+
+    int sum = 0;
+    int count = 0;
+    if (symbol.dim == 1) {
+        for (int i = 0; i < symbol.size1; i++) {
+            sum += symbol.base[i];
+            count++;
+        }
+    } else if (symbol.dim == 2) {
+        for (int i = 0; i < symbol.size1; i++) {
+            for (int j = 0; j < symbol.size2; j++) {
+                sum += symbol.base[i * symbol.size2 + j];
+                count++;
+            }
+        }
+    }
+
+    *retval.base = sum / count;
+    return retval;
 }
 
 refera_symbol_t refera_diag(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+
+    if (symbol.dim != 2) {
+        fprintf(stderr, "Error: Only 2D arrays have diagonals.\n");
+        retval.base = NULL;
+        return retval;
+    }
+
+    int size = (symbol.size1 < symbol.size2) ? symbol.size1 : symbol.size2;
+    retval.dim = 1; 
+    retval.size1 = size;
+    retval.base = malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        retval.base[i] = symbol.base[i * symbol.size2 + i];
+    }
+
+    return retval;
 }
 
 void refera_print(refera_symbol_t symbol)
