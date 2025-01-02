fb4fa366e7e506994c9cba5fb38b9ece30bdad47
Ahmet Hakan Candar <ahmethakan.candar@std.yeditepe.edu.tr>
2025-01-02 14:54:32 +0300

    Implement unit tests for functions

--- src/functions.c
+++ src/functions.c
@@ -29,28 +29,95 @@
 
 refera_symbol_t refera_sum(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+    retval.dim = 0; 
+    retval.base = malloc(sizeof(int));
+    retval.size1 = 0;
+    retval.size2 = 0;
+
+    int sum = 0;
+    if (symbol.dim == 1) {
+        for (int i = 0; i < symbol.size1; i++) {
+            sum += symbol.base[i];
+        }
+    } else if (symbol.dim == 2) {
+        for (int i = 0; i < symbol.size1; i++) {
+            for (int j = 0; j < symbol.size2; j++) {
+                sum += symbol.base[i * symbol.size2 + j];
+            }
+        }
+    }
+
+    *retval.base = sum;
+    return retval;
 }
 
 refera_symbol_t refera_aver(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+    retval.dim = 0; 
+    retval.base = malloc(sizeof(int));
+    retval.size1 = 0;
+    retval.size2 = 0;
+
+    int sum = 0;
+    int count = 0;
+    if (symbol.dim == 1) {
+        for (int i = 0; i < symbol.size1; i++) {
+            sum += symbol.base[i];
+            count++;
+        }
+    } else if (symbol.dim == 2) {
+        for (int i = 0; i < symbol.size1; i++) {
+            for (int j = 0; j < symbol.size2; j++) {
+                sum += symbol.base[i * symbol.size2 + j];
+                count++;
+            }
+        }
+    }
+
+    *retval.base = sum / count;
+    return retval;
 }
 
 refera_symbol_t refera_diag(refera_symbol_t symbol)
 {
-	refera_symbol_t retval;
-	// TODO
-	return retval;
+    refera_symbol_t retval;
+
+    if (symbol.dim != 2) {
+        fprintf(stderr, "Error: Only 2D arrays have diagonals.\n");
+        retval.base = NULL;
+        return retval;
+    }
+
+    int size = (symbol.size1 < symbol.size2) ? symbol.size1 : symbol.size2;
+    retval.dim = 1; 
+    retval.size1 = size;
+    retval.size2 = 0;
+    retval.base = malloc(size * sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        retval.base[i] = symbol.base[i * symbol.size2 + i];
+    }
+
+    return retval;
 }
 
 void refera_print(refera_symbol_t symbol)
 {
-	// TODO: Check if array, if so, print all elements
-	int value = *symbol.base;
-	printf("%d\n", value);
+    if (symbol.dim == 0) {
+        printf("%d\n", *symbol.base);
+    } else if (symbol.dim == 1) {
+        for (int i = 0; i < symbol.size1; i++) {
+            printf("%d ", symbol.base[i]);
+        }
+        printf("\n");
+    } else if (symbol.dim == 2) {
+        for (int i = 0; i < symbol.size1; i++) {
+            for (int j = 0; j < symbol.size2; j++) {
+                printf("%d ", symbol.base[i * symbol.size2 + j]);
+            }
+            printf("\n");
+        }
+    }
 }
